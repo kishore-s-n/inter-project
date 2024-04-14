b9985d14276ca47e00d1975bdf6a20b1c538893a
kishore-s-n~ <snksnk33322@gmail.com~>
2024-04-14 11:11:44 +0530

    feat: completed sign in and sign up page

--- src/Components/AuthFooter.jsx
+++ src/Components/AuthFooter.jsx
@@ -1,6 +1,7 @@
 import Button from "./Button"
 import { Link } from "react-router-dom"
-
+import GoogleLogo from "../assets/googlelogo.png"
+import FbLogo from "../assets/fblogo.png"
 const AuthFooter = (props) => {
     return (
         <div className="w-full flex flex-col justify-center items-center">
@@ -12,8 +13,18 @@ const AuthFooter = (props) => {
                 <span className="mx-4 text-black font-semibold">or</span>
                 <hr className="flex-grow border-gray-500 w-full" />
             </div>
+            <div className="">
+                <i></i>
+            </div>
+            <div className="flex gap-10 my-7">
+                <div className="border-black border-4 p-3 rounded-xl">
+                    <img src={GoogleLogo} alt="" className="h-10"/>
+                </div>
+                <div className="border-black border-4 p-3 rounded-xl">
+                    <img src={FbLogo} alt="" className="h-10"/>
+                </div>
+            </div>
             <Link to={props.to} className='underline'>{props.linktxt}</Link>
-
         </div>
     )
 }
